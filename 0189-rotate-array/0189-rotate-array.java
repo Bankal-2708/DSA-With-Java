c346830fffd7635c2b3df5fe57810a0a70e5fcdf
Bankal mavi <bankalmavi1@gmail.com>
2026-07-22 13:12:38 +0530

    Time: 0 ms (100%), Space: 61.5 MB (25.17%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,18 +1,37 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
-        int[] newArr = new int[nums.length * 2];
 
-        for (int i = 0; i < nums.length; i++) {
-            newArr[i] = nums[i];
-            newArr[i + nums.length] = nums[i];
+    public void rev (int[] nums, int left, int right) {
+       while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
         }
+    }
+
+    public void rotate(int[] nums, int k) {
+        // int[] newArr = new int[nums.length * 2];
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     newArr[i] = nums[i];
+        //     newArr[i + nums.length] = nums[i];
+        // }
+
+        // k = k % nums.length;
+
+        // int start = nums.length - k;
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     nums[i] = newArr[start + i];
+        // }
+
+        // reversing 
 
         k = k % nums.length;
+        rev(nums, 0, nums.length-1);
+        rev(nums, 0, k - 1);
+        rev(nums, k, nums.length - 1);
 
-        int start = nums.length - k;
-
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = newArr[start + i];
-        }
     }
 }
