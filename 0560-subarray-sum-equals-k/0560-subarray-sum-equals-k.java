080c5ea78a014b3a4c5a13c4ba4e19f2ac325fed
Bankal mavi <bankalmavi1@gmail.com>
2026-07-24 21:21:29 +0530

    Time: 23 ms (93.71%), Space: 49.1 MB (11.98%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,25 +1,24 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        int prefix = 0;
+        int count = 0;
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        map.put(0, 1);
+        map.put(0,1);
 
-        int prefixSum = 0;
-        int count = 0;
+        for (int n : nums) {
+            prefix += n;
 
-        for (int num : nums) {
-
-            prefixSum += num;
-
-            if (map.containsKey(prefixSum - k)) {
-                count += map.get(prefixSum - k);
+            if (map.containsKey(prefix - k)) {
+                count += map.get(prefix - k); // add the frequency
             }
+            
+            map.put(prefix, map.getOrDefault(prefix, 0)+1); //If the prefix sum already exists, we increase its frequency by 1. Otherwise, we insert it with a frequency of 1.
 
-            map.put(prefixSum,
-                    map.getOrDefault(prefixSum, 0) + 1);
+
         }
-
         return count;
     }
+    
 }
