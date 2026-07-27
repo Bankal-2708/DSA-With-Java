11e20b61ba59f99a1c4efe7e7274ac83a3ac91c0
Bankal mavi <bankalmavi1@gmail.com>
2026-07-27 21:07:15 +0530

    Time: 0 ms (100%), Space: 43.1 MB (33.12%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,51 @@
+class Solution {
+
+    
+    public boolean canSplit(int[] nums, int k, int maxSum) {
+
+        int subArrays = 1;
+        int currentSum = 0;
+
+        for (int num : nums) {
+
+        
+            if (currentSum + num > maxSum) {
+                subArrays++;
+                currentSum = num;
+
+                if (subArrays > k) {
+                    return false;
+                }
+            } else {
+                currentSum += num;
+            }
+        }
+
+        return true;
+    }
+
+    public int splitArray(int[] nums, int k) {
+
+        int left = 0;
+        int right = 0;
+
+         
+        for (int num : nums) {
+            left = Math.max(left, num);
+            right += num;
+        }
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (canSplit(nums, k, mid)) {
+                 right = mid;
+            } else {
+                 left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
